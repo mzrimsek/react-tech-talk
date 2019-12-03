f795263f974142f3d6a9d92785b01b5882fc47a3
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2019-12-03 11:26:33 -0500

    Updates

---
+++ precache-manifest.00de6fb0a5aca3cebc1ef343b3d8f7eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a9497e22f4b9aaa2b567217cf69362",
+    "url": "/react-tect-talk/index.html"
+  },
+  {
+    "revision": "cbc998b23713f6e3d908",
+    "url": "/react-tect-talk/static/css/main.a6dbb355.chunk.css"
+  },
+  {
+    "revision": "b05fd085b633902bf1d3",
+    "url": "/react-tect-talk/static/js/2.54467b91.chunk.js"
+  },
+  {
+    "revision": "cbc998b23713f6e3d908",
+    "url": "/react-tect-talk/static/js/main.cfc432f0.chunk.js"
+  },
+  {
+    "revision": "2c41c363b65e75790e24",
+    "url": "/react-tect-talk/static/js/runtime-main.69b4b2f0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-tect-talk/static/media/logo.25bf045c.svg"
+  }
+]);
